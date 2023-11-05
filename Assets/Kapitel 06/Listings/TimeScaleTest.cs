9500a40e448869a71e309f13695cf48ab78c90d7
Max Schlosser <mschlos3@hs-mittweida.de>
2023-11-05 12:14:12 +0100

    Remove SampleScene, add TimeScale and Vector2

---
+++ Assets/Kapitel 06/Listings/TimeScaleTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TimeScaleTest : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ Assets/Kapitel 13/Listings/Vector2Test.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Vector2Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Vector2 vector2 = new Vector2(5f, 2f);
+        Vector3 vector3 = vector2;
+        Debug.Log(vector3);
+    }
+}
